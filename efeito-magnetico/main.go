55ca79131ff2354753f9c710b7bb6ef245fe1bf6
Isaias da Silva <i.carvalhodasilva@dxc.com>
2021-05-02 01:39:59 -0300

    new dojo

---
+++ efeito-magnetico/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.dxc.com/clarobr/daigoro/efeito-magnetico/dojo"
+)
+
+func main() {
+	points := make([][]string, 10, 10)
+	m := dojo.NewMatrice(points)
+	m.FillPoints(10)
+	""
+
+}
